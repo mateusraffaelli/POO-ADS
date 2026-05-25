4c7069b20c097d2e5afeda8bab53baff3887a51e
Mateus Raffaelli <mattewscastro@gmail.com>
2026-05-25 18:25:58 -0300

    Alterando o lab-05

--- lab-05/ex-01/src/main/java/ads/poo/Main.java
+++ lab-05/ex-01/src/main/java/ads/poo/Main.java
@@ -7,7 +7,8 @@ public class Main {
     static void main(String[] args) {
         Display display = new Display();
         Relogio relogio1 = new Relogio();
-        Relogio relogio2 = new Relogio(2026, Month.AUGUST, 15, 14, 30,10);
+        Relogio relogio2 = new Relogio(1, 3,5);
+
 
     }
 }
--- lab-05/ex-01/src/main/java/ads/poo/Relogio.java
+++ lab-05/ex-01/src/main/java/ads/poo/Relogio.java
@@ -1,31 +1,42 @@
 package ads.poo;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.Month;
 import java.util.ArrayList;
 
 public class Relogio {
     private ArrayList<Display> displays = new ArrayList<>();
-    public LocalDateTime horario;
+    private LocalTime horario;
+    private int horas;
+    private int minutos;
+    private int segundos;
 
     public Relogio(){
-        this.horario = LocalDateTime.now();
-
+        this.horario = LocalTime.now();
+        adicionando();
     }
 
-    public Relogio(int ano, Month mes, int dia, int hora, int minuto, int segundo){
-        this.horario = LocalDateTime.of(ano, mes, dia, hora, minuto);
+    public Relogio(int hora, int minuto, int segundo){
+        this.horario = LocalTime.of(hora, minuto, segundo);
+        adicionando();
+    }
+
+    public void adicionando(){
+        this.horas = horario.getHour();
+        this.minutos = horario.getMinute();
+        this.segundos = horario.getSecond();
 
         for (int i = 0; i < 6; i++) {
             displays.add(new Display());
         }
 
-        displays.get(0).adicionar(hora);
-        displays.get(1).adicionar(hora);
-        displays.get(2).adicionar(minuto);
-        displays.get(3).adicionar(minuto);
-        displays.get(4).adicionar(segundo);
-        displays.get(5).adicionar(segundo);
+        displays.get(0).adicionar(horas/10);
+        displays.get(1).adicionar(horas%10);
+        displays.get(2).adicionar(minutos/10);
+        displays.get(3).adicionar(minutos%10);
+        displays.get(4).adicionar(segundos/10);
+        displays.get(5).adicionar(segundos%10);
     }
 
 
